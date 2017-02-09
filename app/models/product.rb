96a7bca0bf2ef2e58b12a5a733973f7926af4d05
Jungmin <jungmin.yjm@gmail.com>
2017-02-09 12:32:25 -0500

    ready to push to master

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
   has_many :comments, dependent: :destroy
+
+  def cart_action(current_user_id)
+  if $redis.sismember "cart#{current_user_id}", id
+    "Remove from"
+  else
+    "Add to"
+  end
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :comments
+
+  def cart_count
+  $redis.scard "cart#{id}"
+  end
 end
