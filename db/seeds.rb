0f3f7e4c75c57a0aec755f044808154460479dea
Jungmin <jungmin.yjm@gmail.com>
2017-02-09 22:15:06 -0500

    beautified

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 Product.destroy_all
 
-Product.create(name: "Cool City 11", price: 15, size: "Free", photo: "http://www.dentons.com/~/media/Images/Website/Background%20Images/Offices/Atlanta/Atlanta_2.jpg")
+Product.create(name: "Pale Green Patchwork Print", price: 15, size: "Medium", photo: "http://img.romwe.com/images/romwe.com/201610/1476753583777691774.jpg")
 Product.create(name: "Seattle", price: 30, size: "Small", photo: "http://www.bekinsmovingandstorage.com/wp-content/uploads/2016/03/SeattleCity2.jpg")
 Product.create(name: "Cool City 22 ", price: 25, size: "Free", photo: "http://www.dentons.com/~/media/Images/Website/Background%20Images/Offices/Atlanta/Atlanta_2.jpg")
 Product.create(name: "Test product", price: 100, size: "Medium", photo: "http://www.bekinsmovingandstorage.com/wp-content/uploads/2016/03/SeattleCity2.jpg")
