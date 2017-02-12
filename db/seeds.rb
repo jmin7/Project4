eec0e97daa3289fd00019adde1204218cbc8fe34
Jungmin <jungmin.yjm@gmail.com>
2017-02-11 20:51:55 -0500

    seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,13 @@
 Product.destroy_all
 
 Product.create(name: "Pale Green Patchwork Print", price: 15, size: "Small / Medium / Large", photo: "http://img.romwe.com/images/romwe.com/201610/1476753583777691774.jpg")
-Product.create(name: "Burgundy Alien Embroidered Crop", price: 30, size: "Small / Medium / Large", photo: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRM7-uq0tUA-1PMPJIVe6wMd6mpoIj160ejiMoGKRyr1pGh7Zi0Lg")
-Product.create(name: "Black Slogan Print", price: 25, size: "Medium / Large", photo: "http://img.romwe.com/images/201701/1485149231262197337.jpg")
 Product.create(name: "Heather Grey Contrast Striped", price: 50, size: "Small / Medium ", photo: "http://img.romwe.com/images/romwe.com/201609/1473123119503280051.jpg")
 Product.create(name: "Grey Bow Embellished Crew Neck", price: 20, size: "Small / Medium", photo: "http://img.romwe.com/images/romwe.com/201609/1474247257168743519.jpg")
 Product.create(name: "Black Letter Lace Up Front", price: 17, size: "Medium", photo: "http://img.romwe.com/images/romwe.com/201609/1473671223945504267.jpg")
 Product.create(name: "Orange Crew Neck", price: 10, size: "Small / Medium", photo: "http://img.romwe.com/images/romwe.com/201609/1473383160550650717.jpg")
+
+Product.create(name: "Mens Style", photo: "https://s-media-cache-ak0.pinimg.com/736x/6e/ee/3b/6eee3b00c8d07cd6cfa0689477a26250.jpg")
+Product.create(name: "Casual", photo: "https://s-media-cache-ak0.pinimg.com/236x/1b/c5/31/1bc531a896e0eb133dd1081759100d22.jpg")
+Product.create(name: "Simple", photo: "https://s-media-cache-ak0.pinimg.com/736x/75/dc/d6/75dcd6e73dbaa920966e0d919fc60dc1.jpg")
+Product.create(name: "This looks good", photo: "https://s-media-cache-ak0.pinimg.com/736x/85/43/65/8543655e93728beb1c083cc000b391b5.jpg")
+Product.create(name: "Cool Sweatshirts", photo: "http://40.media.tumblr.com/a8368afa984f75cd6b3e321d31414649/tumblr_mfv0sl6tMp1rldsgeo1_500.jpg")
